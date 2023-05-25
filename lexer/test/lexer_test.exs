ca8f5a4b7236c9ae619da3c9a911ef962b1a18e4
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-05-24 22:35:28 -0600

    Add function to read a line and convert tokens to spans

--- lexer/test/lexer_test.exs
+++ lexer/test/lexer_test.exs
@@ -2,7 +2,10 @@ defmodule LexerTest do
   use ExUnit.Case
   doctest Lexer
 
-  test "greets the world" do
-    assert Lexer.hello() == :world
+  test "arithmetic expressions" do
+    assert Lexer.highlight_line("1 + 2", []) == "<span class=\"number\">1</span> <span class=\"operator\">+</span> <span class=\"number\">2</span>"
+    assert Lexer.highlight_line("3 * 2", []) == "<span class=\"number\">3</span> <span class=\"operator\">*</span> <span class=\"number\">2</span>"
+    assert Lexer.highlight_line("4 / 2", []) == "<span class=\"number\">4</span> <span class=\"operator\">/</span> <span class=\"number\">2</span>"
+    assert Lexer.highlight_line("5 - 2", []) == "<span class=\"number\">5</span> <span class=\"operator\">-</span> <span class=\"number\">2</span>" 
   end
 end
