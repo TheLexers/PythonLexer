5ab5b8d2333ae9f35c9b6782587b17c9dfe2f4d8
juanpaRdeCh <juanpablo.ruiz@live.com>
2023-05-25 19:39:44 -0500

    html body for elixir main code

---
+++ Planeacion/htmlBody.exs
@@ -0,0 +1,25 @@
+
+defp build_html_content(FormatText) do
+  html = ~s(<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Syntax Highlighter</title>
+      <style>
+        .keywords { color: green; }
+        .comments { color: grey; }
+        .operators { color: orange; }
+        .numbers { color: purple; }
+        .strings { color: green; }
+        .brackets { color: orange; }
+      </style>
+    </head>
+    <body>
+      <pre>#{FormatText}</pre>
+    </body>
+    </html>)
+
+  html
+end
